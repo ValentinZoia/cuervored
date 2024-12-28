38249ad4302503dd5d5763650ce2a66bfbf2335d
ValentinZoia <valentinzoia@gmail.com>
2024-12-28 16:46:25 -0300

    change some colors from dark mode

--- src/components/dashboard/NavBar/NavBar.tsx
+++ src/components/dashboard/NavBar/NavBar.tsx
@@ -15,7 +15,7 @@ export default async  function NavBar() {
     <>
       
 
-      <nav className=" sticky top-0 z-50 bg-primary text-primary-foreground p-4">
+      <nav className=" sticky top-0 z-50 bg-blueSanlorenzo text-primary-foreground p-4">
         <div className="container mx-auto flex items-center justify-between">
           <Link href="/dashboard" className="text-xl font-bold">
             LOGO
