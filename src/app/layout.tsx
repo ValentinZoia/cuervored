cf991c722e4eea2594e394272af4445fd418c46c
ValentinZoia <valentinzoia@gmail.com>
2025-01-02 23:57:04 -0300

    refactory: change title from metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+      </head>
       <body className={inter.className}>
         <Providers>
           {children}
