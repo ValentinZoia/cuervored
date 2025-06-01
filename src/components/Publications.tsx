a69521c093b61be87b11f537573a49e62ee18331
ValentinZoia <valentinzoia@gmail.com>
2025-06-01 00:41:56 -0300

    refactory(use the HandlerErrorBoundaty and Suspense to wrap PostList, because it use useSuspenseInfiniteQuery. And use PostList instead of PostForYou and PostFollowing)

--- src/components/Publications.tsx
+++ src/components/Publications.tsx
@@ -1,46 +1,49 @@
-
-import React from "react";
-import { Card, CardContent } from "./ui/card";
-import PostForYou from "./Post/PostForYou";
-import NewPost from "./NewPost/NewPost";
-import { auth } from "@/auth";
+"use client";
+import {Suspense} from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
-import PostFollowing from "./Post/PostFollowing";
+import { HandlerErrorBoundary } from "./Post/PostsErrorBoundary";
+import PostList from './Post/PostList'
+import SkeletonPost from "./Post/SkeletonPost";
+import { getPostsFollowing, getPostsForYou } from "@/data/posts";
+import { PostFeedType } from "@/types/Post";
 
 
 
 
-
-
-export default async function Publications() {
-  const session = await auth();
+export default function Publications() {
+  
 
    
   
 
   return (
     <>
-      <Card className="relative z-10 max-w-[680px] md:w-[680px] lg:w-[680px] bg-transparent border-none shadow-none  sm:mx-auto">
-        <CardContent>
-          <NewPost session={session}/>
+      
           <div className="w-full z-50 min-w-0 space-y-5 ">
-            <Tabs defaultValue="for-you" className="z-50">
+            <Tabs defaultValue={PostFeedType.FOR_YOU} className="z-50">
           <TabsList className="sticky top-[82px] z-50  px-0 flex  space-x-4 bg-card border-[1px] border-border rounded-lg  ">
-            <TabsTrigger className="w-1/2 " value="for-you">Para ti</TabsTrigger>
-            <TabsTrigger className="w-1/2 " value="following">Siguiendo</TabsTrigger>
+            <TabsTrigger className="w-1/2 " value={PostFeedType.FOR_YOU}>Para ti</TabsTrigger>
+            <TabsTrigger className="w-1/2 " value={PostFeedType.FOLLOWING}>Siguiendo</TabsTrigger>
           </TabsList>
-          <TabsContent value="for-you">
-            <PostForYou/>
+          <TabsContent value={PostFeedType.FOR_YOU}>
+            <HandlerErrorBoundary>
+              <Suspense fallback={<SkeletonPost />}>
+              <PostList queryKey={PostFeedType.FOR_YOU} fetchFn={getPostsForYou} />
+              </Suspense>
+            </HandlerErrorBoundary>
           </TabsContent>
-          <TabsContent value="following">
-            <PostFollowing/>
+          <TabsContent value={PostFeedType.FOLLOWING}>
+            <HandlerErrorBoundary>
+              <Suspense fallback={<SkeletonPost />}>
+              <PostList queryKey={PostFeedType.FOLLOWING} fetchFn={getPostsFollowing} />
+              </Suspense>
+            </HandlerErrorBoundary>
           </TabsContent>
         </Tabs>
           </div>
           
           
-        </CardContent>
-      </Card>
+        
     </>
   );
 }
