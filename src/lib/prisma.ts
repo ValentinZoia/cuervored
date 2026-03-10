d2bcffdfd13326471aab467415b622a874840c66
ValentinZoia <valentinzoia@gmail.com>
2026-03-10 00:43:30 +0000

    fix: remove engineType client, keep binaryTargets only

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,6 +10,10 @@ declare const globalThis: {
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma;
-
 if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+
+process.on("beforeExit", (): void => {
+    prisma.$disconnect();
+});
+
+export default prisma;
